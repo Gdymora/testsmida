d6da946f41e9d014cebbcef33e1619c74d950a8e
Alexandr <alekspetrofint@gmail.com>
2021-02-03 20:57:54 +0200

    REST API and JSON.parse

--- server/index.js
+++ server/index.js
@@ -1,25 +1,30 @@
 require('dotenv').config()
 
 const express = require("express");
- 
+
 const app = express();
- 
+const fs = require('fs');
+const dataPath = './server/model/test_data.json';
+
 app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+       
+        return res.send(JSON.parse(data));
+    });
 });
- 
+
 app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
+    return res.send('Received a POST HTTP method');
 });
- 
+
 app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
+    return res.send('Received a PUT HTTP method');
 });
- 
+
 app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
+    return res.send('Received a DELETE HTTP method');
 });
- 
+
 app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
+    console.log(`Example app listening on port ${process.env.PORT}!`),
 );
